8cfb9ab6816d79970fefea785937dfcef11738a2
xianyuyu <1208374029@qq.com>
2023-08-27 23:11:33 +0800

    feat:添加事件监听

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import "element-plus/theme-chalk/el-message.css";
 const audio = ref<HTMLAudioElement>();
 
 // 获取id
-const params = ref<{ id?: string; [key: string]: any }>(
+const params = ref<{ id?: string;[key: string]: any }>(
   getUrlParams(location.search)
 );
 // 加载
@@ -40,7 +40,59 @@ const coverAction = ref<"paused" | "running">("running");
 // 控制播放器字段
 const playerConfig = reactive({
   // 控制播放
-  play: true,
+  play: false,
+  // 控制是否给封面挂载动画
+  reAnimation: false
+});
+
+// 播放器事件
+const playerEvents = reactive({
+  // 控制播放|暂停
+  c_play() {
+    playerConfig.play = !playerConfig.play;
+  },
+  // 重播
+  c_replay() {
+    audio.value!.currentTime = 0;
+    playerConfig.reAnimation = true
+    setTimeout(() => playerConfig.reAnimation = false, 100);
+  }
+});
+
+// 播放器原生事件
+const audioEvents = reactive({
+  // 当音乐缓存到可以播放时触发
+  canplaythrough() {
+    playerConfig.play = true
+  },
+  // 当音乐播放完毕触发
+  ended() {
+    playerConfig.reAnimation = true
+    playerConfig.play = false
+  },
+})
+
+// 监听控制字段
+watchEffect(() => {
+  // 监听播放
+  if (playerConfig.play) {
+    try {
+      audio.value?.play()
+      // 当音乐没有正确播放则不进行操作
+      if (audio.value?.paused) {
+        playerConfig.play = false
+        return
+      }
+      coverAction.value = "running";
+      if (playerConfig.reAnimation) {
+        playerConfig.reAnimation = false
+      }
+    }
+    catch { }
+  } else {
+    coverAction.value = "paused";
+    audio.value?.pause()
+  }
 });
 
 // 获取音乐参数
@@ -67,26 +119,11 @@ const getMusic = async () => {
     }
   } catch (e) {
     console.error(e);
+
   } finally {
-    loading.value = false;
+    loading.value = false
   }
 };
-
-// 事件
-const playerEvents = reactive({
-  c_play() {
-    playerConfig.play = !playerConfig.play;
-  },
-});
-
-// 监听控制字段
-watchEffect(() => {
-  if (playerConfig.play) {
-    coverAction.value = "running";
-  } else {
-    coverAction.value = "paused";
-  }
-});
 
 onMounted(() => {
   const { id } = params.value;
@@ -100,59 +137,38 @@ onMounted(() => {
 </script>
 
 <template>
-  <div
-    class="container"
-    v-loading="loading"
-    :element-loading-text="loadingTips"
-  >
+  <div class="container" v-loading="loading" :element-loading-text="loadingTips">
     <div class="player">
       <div class="title">
         {{ musicData.title || "无歌名" }} --
         {{ musicData.author || "无歌手" }}
       </div>
-      <div class="cover" :class="{ image: musicData.pic.length > 0 }"></div>
+      <div class="cover" :class="{ image: musicData.pic.length > 0, reAnimation: playerConfig.reAnimation }"></div>
       <div class="progress"></div>
       <div class="control">
-        <play
-          theme="filled"
-          size="50"
-          fill="#333"
-          v-if="!playerConfig.play"
-          @click="playerEvents.c_play"
-        />
-        <pause-one
-          theme="filled"
-          size="50"
-          fill="#333"
-          v-else
-          @click="playerEvents.c_play"
-        />
-        <replay-music theme="filled" size="50" fill="#333" />
+        <play theme="filled" size="50" fill="#333" v-if="!playerConfig.play" @click="playerEvents.c_play" />
+        <pause-one theme="filled" size="50" fill="#333" v-else @click="playerEvents.c_play" />
+        <replay-music theme="filled" size="50" fill="#333" @click="playerEvents.c_replay" />
       </div>
       <div class="lrc">
         <div class="lrcBox">
-          <div
-            class="lrcItem"
-            :class="{ select: true }"
-            v-for="(l, index) in lrc"
-            :key="index"
-            :id="l.t"
-          >
+          <div class="lrcItem" :class="{ select: true }" v-for="(l, index) in lrc" :key="index" :id="l.t">
             {{ l.c }}
           </div>
         </div>
       </div>
     </div>
 
-    <audio ref="audio" :src="musicData.url" preload="auto" autoplay></audio>
+    <audio ref="audio" :src="musicData.url" preload="auto" @canplaythrough="audioEvents.canplaythrough"
+      @ended="audioEvents.ended"></audio>
   </div>
 </template>
 
 <style scoped lang="scss">
 .container {
-  max-height: 97vh;
+  max-height: 100vh;
   width: 100vw;
-  height: 97vh;
+  height: 100vh;
   max-width: 100vw;
   box-sizing: border-box;
   position: relative;
@@ -172,6 +188,7 @@ onMounted(() => {
       height: 3rem;
       line-height: 3rem;
     }
+
     .cover {
       width: 18rem;
       height: 18rem;
@@ -182,6 +199,10 @@ onMounted(() => {
       border-radius: 50%;
       position: relative;
       margin-top: 1rem;
+
+      &.reAnimation {
+        animation: none;
+      }
 
       &::before {
         content: "";
@@ -210,6 +231,7 @@ onMounted(() => {
         background-size: cover;
       }
     }
+
     .control {
       display: flex;
       justify-content: space-evenly;
@@ -217,19 +239,19 @@ onMounted(() => {
     }
 
     .lrc {
-      font-size: 10px;
-      height: 14em;
+      height: 14rem;
       margin-top: 2rem;
       overflow: hidden;
 
       .lrcBox {
-        transform: translateY(64px);
+        transform: translateY(7.1rem);
+
         .lrcItem {
           text-align: center;
           color: #00cf4f;
 
           &.select {
-            font-size: 1.2em;
+            font-size: 1.2rem;
             color: #0353e7;
           }
         }
@@ -242,6 +264,7 @@ onMounted(() => {
   0% {
     transform: rotate(0);
   }
+
   100% {
     transform: rotate(360deg);
   }
